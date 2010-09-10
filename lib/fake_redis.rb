b1590b70c7b271047677a59930dc3f3eca319117
Tyler Jennings <tyle.jennings@gmail.com>
2010-09-10 15:41:00 -0500

    zadd, zrange, zrevrange added to FakeRedis + specs

--- lib/fake_redis.rb
+++ lib/fake_redis.rb
@@ -3,11 +3,16 @@ class FakeRedis
     @data = {}
   end
   def zadd(key, score, message)
-    @data[key] = [score, message]
+    @data[key] = (@data[key] || [])
+    @data[key] << [score, message]
+  end
+
+  def zrange(key, start_idx, end_idx)
+    return unless @data[key]
+    @data[key].sort_by{|s, v| s}.map{|s, v| v}[start_idx .. end_idx]
   end
 
   def zrevrange(key, start_idx, end_idx)
-    return unless value = @data[key]
-    value[start_idx, end_idx]
+    zrange(key, start_idx, end_idx).try(:reverse)
   end
 end
---
+++ spec/lib/fake_redis_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "a fake redis implementaiton for testing" do
+  before do 
+    @fake_redis = FakeRedis.new
+  end
+
+  it "supports sorted sets" do 
+    @fake_redis.zadd("k", 2, "b")
+    @fake_redis.zadd("k", 3, "c")
+    @fake_redis.zadd("k", 1, "a")
+
+    @fake_redis.zrange("k", 0, -1).should == ["a", "b", "c"]
+    @fake_redis.zrevrange("k", 0, -1).should == ["c", "b", "a"]
+  end
+end
