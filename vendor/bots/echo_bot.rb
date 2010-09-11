8ddc80a5151c411a9c5054e297a3571eee473aa6
Dojo User <dojouser@Obtiva-Coding-Dojo-2.local>
2010-09-11 15:12:58 -0500

    refactored the echo bot and the base bot and cleaned up the tests

--- vendor/bots/echo_bot.rb
+++ vendor/bots/echo_bot.rb
@@ -1,48 +1,14 @@
-require File.expand_path('../../../config/boot', __FILE__)
-Bundler.require(:bot) if defined?(Bundler)
+require File.expand_path("../bot_base", __FILE__)
 
-class EchoBot
-  include HTTParty
-  headers 'Accept' => '*/*'
+class EchoBot < BotBase
   
-  attr_accessor :username, :last_updated, :options
-  
-  def initialize(username, options = {})
-    self.username = username
-    self.last_updated = 0
-    self.options = options
-  end
-  
-  def run
-    while true
-      new_messages = pull
-      new_messages.each do |message|
-        message_user = message["username"]
-        message_text = message["message"]
-        
-        next if message_user == username
-        
-        puts %Q{Echoing "#{message_text}" from #{message_user}}
-        push(message_text)
-      end
-      sleep 0.5
+  def respond(message_user, message_text)
+    if message_user == username
+      nil
+    else
+      puts %Q{Echoing "#{message_text}" from #{message_user}}
+      message_text
     end
-  end
-  
-  def pull
-    payload = self.class.get("http://localhost:3000/chat/pull/#{last_updated}")
-    self.last_updated = payload["time"]
-    payload["delta"]
-  end
-  
-  def push(message)
-    push_attrs = {:username => username, :message => message}    
-    self.class.post("http://localhost:3000/chat/push", {:body => push_attrs})
-  end
-  
-  def self.run!(username, options = {})
-    bot = self.new(username, options)
-    bot.run
   end
 end
 
