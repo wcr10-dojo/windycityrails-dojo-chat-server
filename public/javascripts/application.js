7cf3e7a751d2d4ac110212031f91a0394a1c6f7d
Dojo User <dojouser@Obtiva-Coding-Dojo-2.local>
2010-09-11 11:49:28 -0500

    Truncate message > 500 and add more link

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,6 +10,11 @@
        }
     });
 
+    $('.show_more').live('click', function(event){
+      $(this).next('.message-extended').toggle();
+      $(this).hide();
+    });
+
     setTimeout(poll, 500);
   });
 
@@ -22,19 +27,29 @@
     $.get('/chat/pull/' + last_sync, function(data) {
       $('#last_sync').text(data['time']);
       $(data['delta']).each(function() {
-        var message = this;
-        var chatMessage = "<b>" + message["username"] + "</b> " + message["message"];
-        var chatElement = $('<p>' + chatMessage + '</p>');
-        if(! WCR.odd) {
-          chatElement.addClass("odd");
-          WCR.odd = true;
-        } else {
-          WCR.odd = false;
-        }
           
-        $('div.chat_window').prepend(chatElement);
+        $('div.chat_window').prepend(formatChatMessage(this));
       });
       setTimeout(poll, 500);
     });
+
+    function formatChatMessage(message) {
+      var chatMessage = "<b>" + message["username"] + "</b> " + message["message"].substring(0, 499);
+
+      if (message["message"].length > 500) {
+        chatMessage += "<span class='extended'> <a href='#' class='show_more'>more</a> <span class='message-extended' style='display:none'>" + message["message"].substring(500, message.length) + "</span></span>";
+      }
+
+      chatMessage = "<p>" + chatMessage + "</p>";
+      var chatElement = $(chatMessage);
+
+      if(! WCR.odd) {
+        chatElement.addClass("odd");
+        WCR.odd = true;
+      } else {
+        WCR.odd = false;
+      }
+      return chatElement;
+    }
   }
 })(jQuery)
