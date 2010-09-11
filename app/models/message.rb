71c3d5ed35107d6d643c731667e879b11569de6d
Dojo User <dojouser@Obtiva-Coding-Dojo-6.local>
2010-09-11 13:59:37 -0500

    google bot added
    josh krueger & joe sak

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message
-
   def self.sanitize!(text)
     HTML::WhiteListSanitizer.new.sanitize(text)
   end
@@ -15,6 +14,10 @@ class Message
       :time_stamp => time_stamp,
       :email => email}.to_json
     RedisClient.redis.zadd("room:default", time_stamp, message_json)
+  end
+  
+  def text
+    @message
   end
 
   private
